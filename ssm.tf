87a077841f73b53fe814bf7ebd55d0c3c6b8fa4d
unknown <ishanmehta3636@gmail.com>
2026-09-11 18:35:36 -0700

    Deploy custom vpc

--- ssm.tf
+++ ssm.tf
@@ -24,6 +24,10 @@ locals {
             name = "/pragra/project-a/subnet4/id"
             value = aws_subnet.subnet4.id
         } 
+        ec2_sg_id = {
+            name = "/pragra/project-a/subnet4/id"
+            value = aws_security_group.ec2_sg_id
+        } 
     }
 }
 
