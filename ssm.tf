9c773ce1379261f0fb4f971768278157e196bb93
unknown <ishanmehta3636@gmail.com>
2026-09-11 18:19:43 -0700

    Deploy custom vpc

---
+++ ssm.tf
@@ -0,0 +1,39 @@
+locals {
+    ssm_parameters = {
+        vpc_id = {
+            name = /pragra/project-a/vpc/id
+            value = aws_vpc.custom_vpc.id
+        }
+
+         publicRT_id = {
+            name = /pragra/project-a/publicRT/id
+            value = aws_route_table.public_rt.id
+        }
+
+         subnet1_id = {
+            name = /pragra/project-a/subnet1/id
+            value = aws_subnet1.subnet1.id
+        }
+        subnet2_id = {
+            name = /pragra/project-a/subnet2/id
+            value = aws_subnet1.subnet2.id
+        }
+        subnet3_id = {
+            name = /pragra/project-a/subnet3/id
+            value = aws_subnet1.subnet3.id
+        }
+        subnet4_id = {
+            name = /pragra/project-a/subnet4/id
+            value = aws_subnet1.subnet4.id
+        }
+    }
+}
+
+resource "aws_ssm_parameter" "resources" {
+  for_each = local.ssm_parameters
+
+  name  = each.value.name
+  type  = "String"
+  value = each.value.value
+}
+
--- terraform.tf
+++ terraform.tf
@@ -8,3 +8,8 @@ terraform {
 
   required_version = ">= 1.2"
 }
+  backend "s3" {
+    bucket = "pragra-tfstate11092026"
+    key    = "env/dev/terraform.tfstate"
+    region = "ca-central-1"
+  }
