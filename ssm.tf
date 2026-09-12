c10ff2d451ce649f7d69e9f8992d3e8a67e7fd34
unknown <ishanmehta3636@gmail.com>
2026-09-11 18:24:58 -0700

    Deploy custom vpc

--- ssm.tf
+++ ssm.tf
@@ -1,29 +1,29 @@
 locals {
     ssm_parameters = {
         vpc_id = {
-            name = /pragra/project-a/vpc/id
+            name = "/pragra/project-a/vpc/id"
             value = aws_vpc.custom_vpc.id
         }
 
          publicRT_id = {
-            name = /pragra/project-a/publicRT/id
+            name = "/pragra/project-a/publicRT/id"
             value = aws_route_table.public_rt.id
         }
 
          subnet1_id = {
-            name = /pragra/project-a/subnet1/id
+            name = "/pragra/project-a/subnet1/id"
             value = aws_subnet1.subnet1.id
         }
         subnet2_id = {
-            name = /pragra/project-a/subnet2/id
+            name = "/pragra/project-a/subnet2/id"
             value = aws_subnet1.subnet2.id
         }
         subnet3_id = {
-            name = /pragra/project-a/subnet3/id
+            name = "/pragra/project-a/subnet3/id"
             value = aws_subnet1.subnet3.id
         }
         subnet4_id = {
-            name = /pragra/project-a/subnet4/id
+            name = "/pragra/project-a/subnet4/id"
             value = aws_subnet1.subnet4.id
         }
     }
